305be835d8885e627f907c8f0c7632dfa1b15606
Leonid Petrov <leonid.petrov@latoken.com>
2020-09-30 12:50:29 +0300

    feat: add cpmmander to task1

--- task1/index.js
+++ task1/index.js
@@ -1,1 +1,15 @@
-console.log('test');
+const commander = require('commander');
+
+const program = new commander.Command();
+
+program.storeOptionsAsProperties(false);
+
+program
+  .option('-s, --shift <shift>', 'a shist')
+  .option('-i, --input <input-file>', 'an input file')
+  .option('-o, --output <output-file>', 'an output file')
+  .option('-a, --action <action>', 'an action encode/decode')
+  .parse(process.argv);
+
+const programOptions = program.opts();
+console.log(programOptions.action);
