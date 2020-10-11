421ca81743766385386f55d5d758f91efc52b608
Leonid Petrov <leonid.petrov@latoken.com>
2020-10-11 21:41:27 +0300

    feat: move create id from user to DB layer

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,13 +1,5 @@
-const uuid = require('uuid');
-
 class User {
-  constructor({
-    id = uuid(),
-    name = 'USER',
-    login = 'user',
-    password = 'P@55w0rd'
-  } = {}) {
-    this.id = id;
+  constructor({ name = 'USER', login = 'user', password = 'P@55w0rd' } = {}) {
     this.name = name;
     this.login = login;
     this.password = password;
