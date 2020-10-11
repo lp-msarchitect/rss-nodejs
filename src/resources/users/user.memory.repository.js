b218da5c80e9144b7bdaab1cff9dae0fe9fadd60
Leonid Petrov <leonid.petrov@latoken.com>
2020-10-11 21:43:43 +0300

    refacotr: rename functions

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,15 +1,15 @@
-const DB = require('../../common/inMemoryDB');
+const DataBase = require('../../common/inMemoryDB');
 
 const getAll = async () => {
-  return DB.getAllUsers();
+  return DataBase.getAllItems('users');
 };
 
 const getById = async id => {
-  return DB.getUserById(id);
+  return DataBase.getItem('users', id);
 };
 
 const create = async user => {
-  return DB.createUser(user);
+  return DataBase.createItem('users', user);
 };
 
 module.exports = { getAll, getById, create };
