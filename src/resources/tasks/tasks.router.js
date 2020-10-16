747ab00864884f9f0d820a06d02f75acb9986319
Leonid Petrov <leonid.petrov@latoken.com>
2020-10-16 04:53:50 +0300

    feat: add tasks request validation

--- src/resources/tasks/tasks.router.js
+++ src/resources/tasks/tasks.router.js
@@ -1,5 +1,25 @@
 const router = require('express').Router({ mergeParams: true });
 const tasksService = require('./tasks.service');
+const Joi = require('joi');
+
+const newTaskSchema = Joi.object({
+  title: Joi.string().required(),
+  order: Joi.string().required(),
+  description: Joi.string().required(),
+  userId: Joi.string().required(),
+  boardId: Joi.string().required(),
+  columnId: Joi.string().required()
+});
+
+const putTaskSchema = Joi.object({
+  title: Joi.string(),
+  order: Joi.string(),
+  description: Joi.string(),
+  userId: Joi.string(),
+  boardId: Joi.string(),
+  columnId: Joi.string(),
+  id: Joi.string().required()
+});
 
 router.route('/').get(async (req, res) => {
   const boardId = req.params.boardId;
@@ -16,7 +36,11 @@ router.route('/:id').get(async (req, res) => {
   }
 });
 
-router.route('/').post(async (req, res) => {
+router.route('/').post(async (req, res, next) => {
+  const { error } = newTaskSchema.validate(req.body);
+  if (error) {
+    return next(error);
+  }
   const newTask = await tasksService.create({
     title: req.body.title,
     order: req.body.order,
@@ -28,7 +52,11 @@ router.route('/').post(async (req, res) => {
   res.json(newTask);
 });
 
-router.route('/:id').put(async (req, res) => {
+router.route('/:id').put(async (req, res, next) => {
+  const { error } = putTaskSchema.validate(req.body);
+  if (error) {
+    return next(error);
+  }
   const task = await tasksService.update({
     title: req.body.title,
     order: req.body.order,
