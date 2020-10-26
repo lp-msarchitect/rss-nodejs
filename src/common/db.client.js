58546f9926663d7d05d377ace8b5efd98639a6cf
Leonid Petrov <leonid.petrov@latoken.com>
2020-10-26 23:25:18 +0300

    feat: add connect to data base

--- src/common/db.client.js
+++ src/common/db.client.js
@@ -1,5 +1,6 @@
 const config = require('./config');
 const mongoose = require('mongoose');
+const User = require('../resources/users/user.model');
 
 const connectToDB = cb => {
   mongoose.connect(config.MONGO_CONNECTION_STRING, {
@@ -10,7 +11,12 @@ const connectToDB = cb => {
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', () => {
-    console.log('Çonnect!');
+    console.log('Connect!');
+    db.dropDatabase();
+    User.insertMany([
+      { name: 'Leonid', login: 'Leonid', password: '123' },
+      { name: 'Vasya', login: 'CyberVasya', password: 'Cyberpunk2077' }
+    ]);
     cb();
   });
 };
