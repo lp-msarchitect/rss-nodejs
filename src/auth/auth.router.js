97e08fc8d6795988710c2dbc80131918e356f3f3
Leonid Petrov <leonid.petrov@latoken.com>
2020-10-28 05:08:12 +0300

    fix: send token in response

--- src/auth/auth.router.js
+++ src/auth/auth.router.js
@@ -4,7 +4,7 @@ const authService = require('./auth.service');
 router.route('/').post(async (req, res) => {
   const token = await authService.generateToken(req.body);
   if (token) {
-    res.status(200).send(token);
+    res.status(200).send({ token });
   } else {
     res.status(403).send('Incorrect login or password');
   }
